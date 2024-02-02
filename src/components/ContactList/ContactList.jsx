21d088f8e8eb3c8befdd3e6f5df78844cc3d3d8d
deathfluffy <mrandreu454@gmail.com>
2024-02-02 20:14:49 +0200

    ++++

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,17 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
+import {  selectVisibleContacts } from '../../redux/selectors';
 import ContactItem from 'components/ContactItem/ContactItem';
-import css from './ContactList.module.css';
-import {  selectContactsAll } from '../../redux/selectors';
-
 
 export default function ContactList() {
-  const contacts = useSelector(selectContactsAll);
+  const contacts = useSelector(selectVisibleContacts);
+ 
 
   return (
-    <>  
-      <ul className={css.list}>
+    <>     
         {contacts.map(contact => (
           <ContactItem key={contact.id} contact={contact} />
-        ))}
-      </ul>
+        ))}     
     </>
   );
 }
